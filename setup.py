50496dc123f5fd84823496fc79c9904992d24d75
Sean Fitzgerald <sean@fitzgeralds.me>
2021-01-17 14:59:50 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='heare-config',
-      version='0.0.1',
+      version='0.0.2',
       description='Heare.io Configuration Utilities',
       author='Sean Fitzgerald',
       author_email='seanfitz@heare.io',
