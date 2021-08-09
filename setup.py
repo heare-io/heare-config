e55b51e1c20a2c83d68b82f57d0634d807f56db0
Sean Fitzgerald <sean@fitzgeralds.me>
2021-08-09 00:31:05 -0700

    Revert "Move readme"
    This reverts commit 60ede363c45558b6f8de41260e1c0127debf80a1.

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
+from typing import Optional
+
+long_description: Optional[str] = None
+if os.path.exists('README.md'):
+    long_description = open('README.md', 'r').read()
 
 setup(name='heare-config',
       version='0.0.6',
       description='Heare.io Configuration Utilities',
+      long_description=str(long_description),
       author='Sean Fitzgerald',
       author_email='seanfitz@heare.io',
       url='https://github.com/heare-io/heare-config',
