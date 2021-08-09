dce73876d7c14ce10bc9ad8113cfa7e09348406d
Sean Fitzgerald <sean@fitzgeralds.me>
2021-08-09 14:09:46 -0700

    add docutils, make valide sript work? (#9)
    * add docutils, make valide sript work?
    
    * quiet output of rts2htm

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -4,4 +4,4 @@ cd "${TOP}"
 pytest "${TOP}"
 pycodestyle "${TOP}"
 mypy "${TOP}"
-rst2html.py --exit-status 3 --report 3 "${TOP}/README.md" > /dev/null
+rst2html.py --exit-status 3 "${TOP}/README.md" > /dev/null
