73956c5a7b68e3ac3e1d2ae0412687c0856bce16
Sean Fitzgerald <sean@fitzgeralds.me>
2021-08-09 00:47:46 -0700

    Simplify long_description

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from distutils.core import setup
 import os
 from typing import Optional
 
-long_description: Optional[str] = None
-if os.path.exists('README.md'):
-    long_description = open('README.md', 'r').read()
+long_description = None
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
 setup(name='heare-config',
       version='0.0.6',
       description='Heare.io Configuration Utilities',
-      long_description=str(long_description),
+      long_description=long_description,
       author='Sean Fitzgerald',
       author_email='seanfitz@heare.io',
       url='https://github.com/heare-io/heare-config',
