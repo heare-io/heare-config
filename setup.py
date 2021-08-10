d00a4fa9aa9dd079133293835cab9990fa6d5e51
Sean Fitzgerald <sean@fitzgeralds.me>
2021-08-09 22:45:59 -0700

    version-bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(name='heare-config',
-      version='0.0.7',
+      version='0.0.7a',
       description='Heare.io Configuration Utilities',
       long_description=long_description,
       long_description_content_type="text/markdown",
