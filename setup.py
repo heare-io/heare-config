3d4c35f358126271aca95702fa1a5861ab6c9940
Sean Fitzgerald <sean@fitzgeralds.me>
2021-08-09 22:50:02 -0700

    finally!
    readme confirmed functional, final version set to v0.0.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(name='heare-config',
-      version='0.0.7a',
+      version='0.0.7',
       description='Heare.io Configuration Utilities',
       long_description=long_description,
       long_description_content_type="text/markdown",
