71073b15a66882df6ba8530cb00b3e0154a92f6e
Sean Fitzgerald <sean@fitzgeralds.me>
2021-08-09 22:37:14 -0700

    :\

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -3,5 +3,5 @@ TOP="$(cd $(dirname $0)/.. && pwd -L)"
 cd "${TOP}"
 pytest "${TOP}"
 pycodestyle "${TOP}"
-mypy "${TOP}"
+mypy "${TOP}/heare" "${TOP}/tests"
 rst2html.py --exit-status 3 "${TOP}/README.md" > /dev/null
